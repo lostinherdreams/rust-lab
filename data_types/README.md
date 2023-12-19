f2d0b8bb4951a80c01f3c1a2df392ca73191aad9
mahya <beheshtimahya11@gmail.com>
2023-12-19 11:08:48 +0330

    data types

---
+++ data_types/README.md
@@ -0,0 +1,54 @@
+# Rust Fundamentals Overview
+
+This Rust code provides a comprehensive overview of fundamental concepts such as variable types, scalar types, compound types, and basic operations in Rust programming.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Usage](#usage)
+- [Concepts Covered](#concepts-covered)
+- [Code Explanation](#code-explanation)
+- [Additional Notes](#additional-notes)
+
+## Introduction
+
+This repository contains Rust code demonstrating various fundamental concepts crucial to understanding Rust programming. It serves as an educational resource to grasp essential concepts like variable types, scalar and compound types, basic operations, and more.
+
+## Usage
+
+To run the code, follow these steps:
+
+1. Ensure Rust is installed on your system.
+2. Clone this repository to your local machine.
+3. Navigate to the directory containing the code.
+4. Run the code using `cargo run`.
+
+## Concepts Covered
+
+### Scalar Types
+
+Detailed explanations and examples are provided for integers, floating-point numbers, booleans, and characters. It covers their range, default types, and operations.
+
+### Variable Types
+
+The code explains Rust's statically-typed nature and demonstrates scenarios where explicit type annotations are necessary, particularly during type conversions.
+
+### Compound Types
+
+The code introduces tuples and arrays, highlighting their fixed lengths, type homogeneity, and access methods for elements.
+
+### Basic Operations
+
+Examples showcase arithmetic operations (addition, subtraction, multiplication, division, remainder), character manipulations, and how to perform these operations on different types.
+
+## Code Explanation
+
+The code within `main()` provides clear examples for each concept discussed. Comments explain the purpose of each block, providing context and explaining the outcomes of the operations performed.
+
+## Additional Notes
+
+- **Error Handling**: While not explicitly covered, Rust emphasizes error handling with the `Result` type, an essential concept for robust programming.
+- **Ownership and Borrowing**: This code does not delve into Rust's ownership and borrowing, which are critical for understanding memory management.
+- **Control Flow**: Control flow statements like `if`, `match`, and loops are not included but are important for writing conditional and iterative code.
+
+Feel free to explore the code and comments for a detailed understanding of Rust's fundamental concepts!
--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -25,28 +25,113 @@ fn main() {
         - characters
     */
 
-    //int//
+    //***int***//
 
-    let unsigned_int = 4000;
-    let signed_int = -4_000;
-    // you can store number 1000 like 1_000 [easier to read]
+      // i32 --> [2^(-31) , 2^31 - 1]
 
-    let hex = 0xff;
-    let oct = 0o77;
-    let bin = 0b1100;
-    let byte = b'A';
-    println!("A is {byte} in ASCII"); // ->65
+      let unsigned_int = 4000;
+      let signed_int = -4_000;
+      // you can store number 1000 like 1_000 [easier to read]
 
+      let hex = 0xff;
+      let oct = 0o77;
+      let bin = 0b1100;
+      let byte = b'A';
+      println!("A is {byte} in ASCII"); // ->65
 
-    //float [f32/f64]//
-
-    //default = f64
-    let x = 2.0; // f64
-    let y: f32 = 3.0; // f32
+      // overflow -> panic in debug mode / 2th complement in release mode
 
 
-    //operation//
-    
+    //***float [f32/f64]***//
+
+      //default = f64
+      let x = 2.0; // f64
+      let y: f32 = 3.0; // f32
+
+
+    //***operation***//
+
+      // addition
+      let sum = 5 + 10;
+
+      // subtraction
+      let difference = 95.5 - 4.3;
+
+      // multiplication
+      let product = 4 * 30;
+
+      // division
+      let quotient = 56.7 / 32.2;
+      let truncated = -5 / 3; // Results in -1
+
+      // remainder
+      let remainder = 43 % 5;
+  
+    //***boolean type***//
+
+      //one byte in size
+      let t = true;
+
+      let f: bool = false; // with explicit type annotation
+
+    //***character type***//
+
+      /*
+        -four bytes in size
+        -represents a Unicode Scalar Value
+        -range from U+0000 to U+D7FF and U+E000 to U+10FFFF
+      */
+      let c = 'z';
+      let z: char = 'ℤ'; // with explicit type annotation
+      let heart_eyed_cat = '😻';
+
+
+////////////////////
+//Coumpound Types//
+//////////////////
+
+    /* 
+      - can group multiple values into one type 
+        + tuples
+        + arrays
+    */
+
+    //***tupple type ***//
+
+      /*
+        - grouping together values with diffrent types 
+        - fixed length
+        - unit :empty tupple
+          +value and type --> ()
+      */
+      let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+      //accesss
+      let tup = (500, 6.4, 1);
+
+      let (x, y, z) = tup; //destructuring
+      println!("The value of y is: {y}");
+
+      let x = tup.0;
+      println!("The value of tup[0] is: {x}");
+
+
+    //***array type***//
+
+      /*
+        - every element of an array must have the same type
+        - fixed length
+        - allocate data on the stack rather than the heap 
+        - for when you know the number of elements dont need to change
+      */
+      let a = [1, 2, 3, 4, 5];
+
+      let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+      let a = [3; 5]; // --> [3,3,3,3,3]
+
+      let first = a[0]; //index out of bound --> panick
 
 
 }
